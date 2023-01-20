7f8714dd91a4ca37afdbcd0d4f6e5e1b1a37beec
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2023-01-20 01:45:03 -0300

    feat: add tableview and people model

---
+++ Star-Wars-API-App/Star-Wars-API-App/Controller/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Star-Wars-API-App
+//
+//  Created by Leonardo Cardoso on 20/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
+        setupTableViewConstraints()
+    }
+    
+    func setupTableViewConstraints() {
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        table.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
+        table.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        table.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell")!
+        return cell
+    }
+    
+    
+}
+
---
+++ Star-Wars-API-App/Star-Wars-API-App/Model/People.swift
@@ -0,0 +1,51 @@
+//
+//  File.swift
+//  Star-Wars-API-App
+//
+//  Created by Leonardo Cardoso on 20/01/23.
+//
+
+import Foundation
+
+
+struct PeopleList: Codable {
+    let results: [People]
+}
+
+struct People: Codable {
+    let name: String
+    let height: Int
+    let mass: Int
+    let hair_color: String
+    let eye_color: String
+    let birth_year: String
+    let gender: String
+    let homeworld: String
+}
+
+
+
+//"        name": "Luke Skywalker",
+//            "height": "172",
+//            "mass": "77",
+//            "hair_color": "blond",
+//            "skin_color": "fair",
+//            "eye_color": "blue",
+//            "birth_year": "19BBY",
+//            "gender": "male",
+//            "homeworld": "https://swapi.dev/api/planets/1/",
+//            "films": [
+//                "https://swapi.dev/api/films/1/",
+//                "https://swapi.dev/api/films/2/",
+//                "https://swapi.dev/api/films/3/",
+//                "https://swapi.dev/api/films/6/"
+//            ],
+//            "species": [],
+//            "vehicles": [
+//                "https://swapi.dev/api/vehicles/14/",
+//                "https://swapi.dev/api/vehicles/30/"
+//            ],
+//            "starships": [
+//                "https://swapi.dev/api/starships/12/",
+//                "https://swapi.dev/api/starships/22/"
+//            ],
