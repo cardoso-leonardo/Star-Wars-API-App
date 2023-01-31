f4de5380691ecb8ed78af543c4a233906f78ce1f
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2023-01-31 13:21:46 -0300

    fix: classes to final classes

--- TheMovieDB-App/TheMovieDB-App/Controllers/TabBar/TMDBTabBarController.swift
+++ TheMovieDB-App/TheMovieDB-App/Controllers/TabBar/TMDBTabBarController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TMDBTabBarController: UITabBarController {
+final class TMDBTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- TheMovieDB-App/TheMovieDB-App/View/TMDBPeopleListView.swift
+++ TheMovieDB-App/TheMovieDB-App/View/TMDBPeopleListView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TMDBPeopleListView: UIView {
+final class TMDBPeopleListView: UIView {
     
     private let viewModel = TMDBPeopleListViewViewModel()
     
