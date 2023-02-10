eb0fcb1e84b5116c534cf6404c8d6c992bc626f4
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2023-02-09 23:26:55 -0300

    feat: organize SceneDelegate willConnectTo method

--- TheMovieDB-App/TheMovieDB-App/Resources/SceneDelegate.swift
+++ TheMovieDB-App/TheMovieDB-App/Resources/SceneDelegate.swift
@@ -15,11 +15,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let window = UIWindow(windowScene: windowScene)
+        window = UIWindow(windowScene: windowScene)
         
-        window.rootViewController = TMDBTabBarController()
-        window.makeKeyAndVisible()
-        self.window = window
+        window?.rootViewController = TMDBTabBarController()
+        window?.makeKeyAndVisible()
     
     }
 
